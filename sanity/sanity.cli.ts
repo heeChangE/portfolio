cd1394a162df9425089feb1c582e4cdb8fc5be35
Yu hee chang <yhc10215@gmail.com>
2023-03-15 12:37:27 +0900

    fixing..

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'lqmtc43z',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
